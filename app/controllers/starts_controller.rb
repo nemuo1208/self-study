f4692029edf8a8fcf9e9746632f1716079568689
nemuo1208 <151502454+nemuo1208@users.noreply.github.com>
2024-01-20 18:05:06 +0900

    indexページ完成

--- app/controllers/starts_controller.rb
+++ app/controllers/starts_controller.rb
@@ -1,4 +1,4 @@
 class StartsController < ApplicationController
   def index
-  end  
+  end
 end
--- test/controllers/starts_controller_test.rb
+++ test/controllers/starts_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class StartsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
